0fc50c1d45d320aedb7c7b5d89bf40f562a7deef
Julia <julia.taro@khk.ee>
2021-11-20 22:57:46 +0200

    5 week

---
+++ src/week5/PersonExt93/Main.java
@@ -0,0 +1,9 @@
+package week5.PersonExt93;
+
+public class Main {
+    public static void main(String[] args) {
+        // write test code here
+        // Person pekka = new Person("Pekka", 15, 2, 1993);
+    }
+
+}
